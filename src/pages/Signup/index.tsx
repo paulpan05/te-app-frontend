8cec11212577565c66fa1a0c025a429b0b0bff44
Amit Bar <amitbar00@gmail.com>
2020-05-16 01:25:02 -0700

    basically finished with signup page, need more styling and must add api calls

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { authActions } from '../../redux/actions';
@@ -10,31 +9,45 @@ import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { rootState } from '../../redux/reducers';
+import styles from '../Login/index.module.scss';
 
 interface SignupProps {
-  dispatch: Dispatch<any>;
+  user: firebase.User | null | undefined,
+  profileImgSrc: string,
+  altContact1Placeholder: string,
+  altContact2Placeholder: string,
+  dispatch: Dispatch<any>
 }
 
-class SignupForm extends Component<{}, { profileImgSrc: string, placeholder: string }> {
-  componentWillMount() {
-    this.setState({ profileImgSrc: blankProfile, placeholder: "Select Alternate..." });
-  }
+const mapStateToProps = (state: rootState) => ({
+  user: state.auth.user,
+});
 
-  render() {
-    return (
+const Signup: React.FC<SignupProps> = ({ user, dispatch }) => {
+  /*let profileImg = await //API call for user profile picture from google goes here// ;
+  if (profileImg === null) {
+    profileImg = blankProfile;
+  }*/
+  const [profileImgSrc, setProfileImgSrc] = useState((user && user.photoURL) ? user.photoURL : blankProfile);
+  const [altContact1Placeholder, setAltContact1Placeholder] = useState("(123) 456-7890");
+  const [altContact2Placeholder, setAltContact2Placeholder] = useState("example@gmail.com");
+
+  return (
+    <div className={styles.page}>
       <Card as={Container} className="mt-4">
         <Form>
           <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Label> {/* TODO is this needed */}
-                <Image src={this.state.profileImgSrc} roundedCircle width="200" height="200" className="mt-2 mb-2" />
-                <Form.Text>Click to Select a Profile Picture</Form.Text>
+            <Form.Group as={Col} md="4">
+              <Form.Label>
+                <Image src={profileImgSrc} roundedCircle width="200" height="200" className="mt-2 mb-2" draggable={false} />
+                <Form.Text className="my-auto text-center">Click to Select a Profile Picture</Form.Text>
                 <Form.File
                 id="upload-profile"
                 accept="image/*"
@@ -42,88 +55,94 @@ class SignupForm extends Component<{}, { profileImgSrc: string, placeholder: str
                 onChange={(e: any) => {
                   if (e.target.files && e.target.files.length === 1 && e.target.files[0]) {
                     console.log(e.target.files[0]);
-                    URL.revokeObjectURL(this.state.profileImgSrc);
-                    this.setState({ profileImgSrc: URL.createObjectURL(e.target.files[0]) });
-                  }
-                }}
-                onLoad={(e: any) => { /* double check this to see if it gets rid of the blob */
-                  if (e.target.files && e.target.files.length === 1 && e.target.files[0]) {
-                    console.log(this.state.profileImgSrc);
-                    URL.revokeObjectURL(this.state.profileImgSrc);
-                    console.log(this.state.profileImgSrc);
+                    URL.revokeObjectURL(profileImgSrc);
+                    setProfileImgSrc(URL.createObjectURL(e.target.files[0]));
                   }
                 }}/>
                 
               </Form.Label>
             </Form.Group>
+
+            <Form.Group as={Col} md="8" className="my-auto">
+              <Form.Row>
+                <Form.Group as={Col} md="4">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    plaintext
+                    readOnly
+                    defaultValue={(user && user.displayName) ? user.displayName : "Name cannot be rendered: error occurred"} />
+                </Form.Group>
+
+                <Form.Group as={Col} md="4">
+                  <Form.Label>Primary Contact</Form.Label>
+                  <Form.Control
+                      plaintext
+                      readOnly
+                      defaultValue={(user && user.email) ? user.email : "Email cannot be retrieved: error occurred"} />
+                </Form.Group>
+              </Form.Row>
+            </Form.Group>
           </Form.Row>
-          
+
           <Form.Row>
             <Form.Group as={Col} md="4">
               <Form.Label>Preferred Name</Form.Label>
               <Form.Control placeholder="Preferred Name" />
               <Form.Text>This will be displayed instead of your first name</Form.Text>
             </Form.Group>
-
-            <Form.Group as={Col} md="4">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control plaintext readOnly defaultValue="First Name goes here" />
-            </Form.Group>
-
-            <Form.Group as={Col} md="4">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control plaintext readOnly defaultValue="Last Name goes here" />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Label as={Col} md="2" className="mb-3">Primary Contact</Form.Label>
-            <Form.Label as={Col} md="2" className="mb-3">Email goes here</Form.Label>
           </Form.Row>
 
           <Form.Label>Alternate Contacts</Form.Label>
           <Form.Row>
             <Form.Group as={Col} md="6">
               <InputGroup
-                as={InputGroup.Prepend}
                 title="Type"
                 id="email or number">
-                <ButtonGroup aria-label="First group">
-                  <Button variant="outline-secondary">Email</Button>
-                  <Button variant="outline-secondary">Phone</Button>
-                </ButtonGroup>
-                <FormControl aria-describedby="emailOrNumber" placeholder="Email" />
+                <ToggleButtonGroup as={InputGroup.Prepend} type="radio" name="options" defaultValue={"phone"}>
+                  <ToggleButton value={"email"} variant="outline-secondary" onClick={() => {
+                    setAltContact1Placeholder("example@gmail.com");
+                  }}>Email</ToggleButton>
+                  <ToggleButton value={"phone"} variant="outline-secondary" onClick={() => {
+                    setAltContact1Placeholder("(123) 456-7890");
+                  }}>Phone</ToggleButton>
+                </ToggleButtonGroup>
+                <FormControl
+                  placeholder={altContact1Placeholder}
+                  defaultValue={(user && user.phoneNumber) ? user.phoneNumber : ""} />
               </InputGroup>
+              <Form.Text>These will be displayed publicly</Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} md="6">
               <InputGroup
-                as={InputGroup.Prepend}
                 title="Type"
                 id="email or number">
-                <ButtonGroup aria-label="First group">
-                  <Button variant="outline-secondary">Email</Button>
-                  <Button variant="outline-secondary">Phone</Button>
-                </ButtonGroup>
-                <FormControl aria-describedby="emailOrNumber" placeholder="Email" />
+                <ToggleButtonGroup as={InputGroup.Prepend} type="radio" name="options" defaultValue={"email"}>
+                  <ToggleButton value={"email"} variant="outline-secondary" onClick={() => {
+                    setAltContact2Placeholder("example@gmail.com");
+                  }}>Email</ToggleButton>
+                  <ToggleButton value={"phone"} variant="outline-secondary" onClick={() => {
+                    setAltContact2Placeholder("(123) 456-7890");
+                  }}>Phone</ToggleButton>
+                </ToggleButtonGroup>
+                <FormControl placeholder={altContact2Placeholder} />
               </InputGroup>
             </Form.Group>
           </Form.Row>
+
           <Form.Row>
-            <Button variant="primary" type="submit" className="mb-2" onClick={() => {
-              // Put for database goes here
-            }}>Continue</Button>
+            <Button variant="primary" type="submit" className="mb-2 mr-2" onClick={() => {
+              // validate forms
+              // API PUT to database
+            }}>Signup</Button>
+            
+            <Button variant="outline-secondary" className="mb-2" onClick={() => {
+              dispatch(authActions.signOut());
+            }}>Cancel</Button>
           </Form.Row>
         </Form>
       </Card>
-    );
-  }
-}
+    </div>
+  )};
 
-const Signup: React.FC<SignupProps> = ({ dispatch }) => (
-  <SignupForm />
-);
-
-
-
-export default connect()(Signup);
+export default connect(mapStateToProps)(Signup);
