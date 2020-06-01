3e70ea48f6b93b57be0343f48bef1df2de762a03
Quy Mac <quylanmac@gmail.com>
2020-06-01 01:29:31 -0700

    profile picture on comment form

--- src/components/CommentBox/CommentForm.tsx
+++ src/components/CommentBox/CommentForm.tsx
@@ -1,11 +1,11 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import styles from './index.module.scss';
 import ProfileImg from '../../assets/img/sarah.png';
 import { v4 as uuidv4 } from 'uuid';
 import { text } from '@fortawesome/fontawesome-svg-core';
-
+import {getUserProfile} from '../../api';
 interface CommentFormProps {
   user: firebase.User | null | undefined;
   onCommentSubmit: Function;
@@ -13,9 +13,9 @@ interface CommentFormProps {
 
 let textData = '';
 
-const CommentForm: React.FC<CommentFormProps> = ({ user, onCommentSubmit }) => {
+const CommentForm: React.FC<CommentFormProps> = ({ user, onCommentSubmit, }) => {
   const [txt, setTxt] = useState('');
-
+  const [myProfile, setMyProfile] = useState<any>();
   const handleTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     textData = event.target.value;
     setTxt(textData);
@@ -34,11 +34,14 @@ const CommentForm: React.FC<CommentFormProps> = ({ user, onCommentSubmit }) => {
     setTxt('');
     textData = '';
   };
-
+  useEffect(() => {
+    getUserProfile(user, undefined, setMyProfile);
+  },[]);
   return (
     <div>
+      {myProfile && 
       <form className={styles.commentForm} onSubmit={handleSubmit}>
-        <img src={ProfileImg} className={styles.authorPicture} alt="author" />
+        <img src={myProfile.picture} className={styles.authorPicture} alt="author" />
         <input
           type="text"
           className={styles.commentInput}
@@ -46,7 +49,7 @@ const CommentForm: React.FC<CommentFormProps> = ({ user, onCommentSubmit }) => {
           value={txt}
           placeholder="Write a Comment... (100 chars limit)"
         />
-      </form>
+      </form>}
     </div>
   );
 };
