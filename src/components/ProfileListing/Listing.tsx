fd723a6ded0ea8d84057dbc7adf0040243616615
Paul Pan <panjunhong05@gmail.com>
2020-06-06 15:41:54 -0700

    Massive refactoring

--- src/components/ProfileListing/Listing.tsx
+++ src/components/ProfileListing/Listing.tsx
@@ -2,15 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Carousel from 'react-bootstrap/Carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faSearch } from '@fortawesome/free-solid-svg-icons';
-import styles from './index.module.scss';
-import GreenShirt from '../../assets/img/GreenTshirt.png';
-import FlowerImg from '../../assets/img/FlowerShirt.png';
-import ViewListing from '../ViewListing/ViewListing';
-import book from '../../assets/img/books.jpg';
-import CustomToggleButton from '../CustomToggleButton';
-import {saveListing, unsaveListing} from '../../api';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
+import ViewListing from '../ViewListing/ViewListing';
+import { saveListing, unsaveListing } from '../../api';
+import styles from './index.module.scss';
 
 interface ListingProps {
   user: firebase.User | null | undefined;
@@ -21,38 +17,46 @@ interface ListingProps {
   postDate: number;
   pictures: string[];
   reloadProfile?: Function;
-  
 }
 
-const Listing: React.FC<ListingProps> = ({title, price, postDate, pictures, userInfo, listingId, user, reloadProfile}) => {
+const Listing: React.FC<ListingProps> = ({
+  title,
+  price,
+  postDate,
+  pictures,
+  userInfo,
+  listingId,
+  user,
+  reloadProfile,
+}) => {
   const [show, setShow] = useState(false);
   const [reload, setReload] = useState(false);
   const [toggled, setToggled] = useState(false);
   function getPictures() {
     return pictures.map((picture) => {
       return (
-      <Carousel.Item onClick={() => setShow(true)}>
-        <img onClick={() => setShow(true)} className={styles.cardImgTop} src={picture} />
-      </Carousel.Item>
-      )
-    })
+        <Carousel.Item onClick={() => setShow(true)}>
+          <img className={styles.cardImgTop} src={picture} alt="card" />
+        </Carousel.Item>
+      );
+    });
   }
   useEffect(() => {
-    if(userInfo != null) {
-      for (let i = 0; i < userInfo.savedListings.length; i++) {
+    if (userInfo != null) {
+      for (let i = 0; i < userInfo.savedListings.length; i += 1) {
         if (userInfo.savedListings[i][0] === listingId) {
           setToggled(true);
           break;
         }
       }
     }
-    if (reload===true) {
+    if (reload === true) {
       if (reloadProfile) {
         reloadProfile(true);
       }
       setReload(false);
     }
-  }, [reload]);
+  }, [reload, listingId, reloadProfile, userInfo]);
 
   return (
     <div className="hoverPointer" style={{ margin: '5%' }}>
@@ -64,40 +68,49 @@ const Listing: React.FC<ListingProps> = ({title, price, postDate, pictures, user
         </div>
         <div className={styles.card_content}>
           <h2 className={styles.card_title}>{title}</h2>
-          <p className={styles.card_text}>${price}</p>
-          <p className={styles.description}>Posted {new Date(postDate).toDateString().split(' ').slice(1).join(' ')}</p>
-          <button onClick = {async () => {setToggled(!toggled)
-            if(toggled === true) {
-              const successUnsave = await unsaveListing(
-                user,
-                listingId,
-                postDate
-              );
-            } else {
-              const successSave = await saveListing(
-                user,
-                listingId,
-                postDate
-              );
-              if (successSave) {
-                toast('This listing has been added to your Saved collection!');
+          <p className={styles.card_text}>
+            <span>$</span>
+            <span>{price}</span>
+          </p>
+          <p className={styles.description}>
+            <span>Posted </span>
+            <span>{new Date(postDate).toDateString().split(' ').slice(1).join(' ')}</span>
+          </p>
+          <button
+            type="submit"
+            onClick={async () => {
+              setToggled(!toggled);
+              if (toggled === true) {
+                await unsaveListing(user, listingId, postDate);
               } else {
-                toast(
-                  'There has been an error while adding this to your saved collection. Please try again.',
-                );
+                const successSave = await saveListing(user, listingId, postDate);
+                if (successSave) {
+                  toast('This listing has been added to your Saved collection!');
+                } else {
+                  toast(
+                    'There has been an error while adding this to your saved collection. Please try again.',
+                  );
+                }
               }
-
-            }
-            
-          }
-          } 
-          className={styles.like_btn}>
-                <FontAwesomeIcon icon={faHeart} size="lg" className={toggled ? styles.heartActive : styles.heartInactive} />
+            }}
+            className={styles.like_btn}
+          >
+            <FontAwesomeIcon
+              icon={faHeart}
+              size="lg"
+              className={toggled ? styles.heartActive : styles.heartInactive}
+            />
           </button>
         </div>
       </div>
       {show && (
-        <ViewListing reloadListing={setReload} listingId={listingId} creationTime={postDate} show={show} setShow={setShow} />
+        <ViewListing
+          reloadListing={setReload}
+          listingId={listingId}
+          creationTime={postDate}
+          show={show}
+          setShow={setShow}
+        />
       )}
     </div>
   );
